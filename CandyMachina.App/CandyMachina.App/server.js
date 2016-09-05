d2b5936e981d266df3a02c52992340d27bb7a6f4
Seabstian Johansson <johansson.sebastian@gmail.com>
2016-09-05 20:12:28 +0200

    Added raspivid module

--- CandyMachina.App/CandyMachina.App/server.js
+++ CandyMachina.App/CandyMachina.App/server.js
@@ -1,6 +1,15 @@
 ﻿var http = require('http');
-var port = process.env.port || 1337;
+var port = process.env.port || 8000;
+
+var raspivid = require('raspivid');
+var fs = require('fs');
+
+var file = fs.createWriteStream(__dirname + '/video.h264');
+var video = raspivid();
+
+
 http.createServer(function (req, res) {
+    video.pipe(file);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end('<video width="320" height="240" autoplay>< source src="/video.h264" type= "video/mp4" >Your browser does not support the video tag.</video>>');
 }).listen(port);
